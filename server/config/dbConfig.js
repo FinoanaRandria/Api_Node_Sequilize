19af44371b9e3dae6d6b2f3aefdebb4dde9e5794
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-08-08 19:36:15 +0200

    config option for sequilize + table initialization

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -8,7 +8,7 @@ module.exports = {
      pool:{
         max:5,
         min:0,
-        auquire:30000,
+        acquire:30000,
         idle:10000
      }
 }
--- server/models/index.js
+++ server/models/index.js
@@ -11,6 +11,35 @@ const sequilize = new Sequelize(
         dialect:dbConfig.dialect,
         operatorsAliases:false,
         
+             pool:{
 
+                max:dbConfig.pool.max,
+                min:dbConfig.pool.min,
+                acquire:dbConfig.pool.acquire,
+                idle:dbConfig.pool.idle
+             }
     }
 )
+
+sequilize.authenticate()
+.then(() =>{
+     console.log('connected');
+})
+.catch(err=>{
+     console.log('Error' + err);
+})
+
+
+const db = {}
+
+db.Sequilize = Sequelize
+db.sequilize = sequilize
+
+
+//la table
+db.product = require('./productModels.js')(sequilize,DataTypes)
+db.reviews = require('./reviewModels.js')(sequilize,DataTypes)
+
+
+//pour ne pas creer la table encore et encore si elle n est pas trouve
+db.sequilize.sync({force:false})
