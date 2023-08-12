ccc2bceb98ec3fac5c44f55c7662ce428d37f349
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-08-12 16:30:44 +0200

    product controller get all product and insert product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -9,3 +9,27 @@ const Review = db.reviews
 //espace de traville
 
 //1 . creation du porduit
+
+
+const addProduct = async(req,res)=>{
+      
+    let info = {
+        title:req.body.title,
+        price:req.body.price,
+        description:req.body.description,
+        published:req.body.published ? req.body.published:false
+    }
+     
+const product = await Product.create(info)
+res.status(200).send(product)
+
+}
+
+// . obtenir tout les produit de la table
+
+const getAllProducts = async(req,res)=>{
+
+ let products = await Product.findAll({})
+ res.status(200).send(products)
+
+}
